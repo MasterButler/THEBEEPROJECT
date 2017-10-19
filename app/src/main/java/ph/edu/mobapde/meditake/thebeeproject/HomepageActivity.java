d989ac0e26c4876eb05f5968ef82d1c352ac5b43
Kyle-Althea <kyle-althea_santos@dlsu.edu.ph>
2017-10-20 04:16:19 +0800

    Better login page

--- app/src/main/java/ph/edu/mobapde/meditake/thebeeproject/HomepageActivity.java
+++ app/src/main/java/ph/edu/mobapde/meditake/thebeeproject/HomepageActivity.java
@@ -1,11 +1,17 @@
 package ph.edu.mobapde.meditake.thebeeproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class HomepageActivity extends AppCompatActivity {
 
@@ -15,6 +21,16 @@ public class HomepageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_homepage);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
+
+    }
+
+    @OnClick(R.id.signin)
+    public void signin(View view) {
+        Intent i = new Intent(this, DrawerActivity.class);
+        startActivity(i);
     }
 
 }
