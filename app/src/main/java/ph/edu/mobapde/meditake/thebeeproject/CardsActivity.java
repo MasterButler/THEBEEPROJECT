d7a18b3f240972f2a7ff105572e243fbaa3bcaae
kendeguia <kendall_deguia@dlsu.edu.ph>
2017-10-20 05:07:04 +0800

    CardsActivity UI

---
+++ app/src/main/java/ph/edu/mobapde/meditake/thebeeproject/CardsActivity.java
@@ -0,0 +1,13 @@
+package ph.edu.mobapde.meditake.thebeeproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cards);
+    }
+}
